7d6bd7769f565fe55a7773648b0e1a1509f40ef6
bjf2015 <bfranzoi@icloud.com>
2016-07-20 09:35:49 -0700

    Finished Extra Exercices

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 // answer to Fizz Buzz
-var numberCount = 1
-for (var numberCount = 1; numberCount <100; numberCount++) {
-	if (numberCount % 5 === 0 && numberCount % 3 === 0){
-		console.log('FizzBuzz')
-	} else if (numberCount % 5 === 0) {
-		console.log('Buzz');
-	} else if (numberCount % 3 === 0){
-		console.log('Fizz');
-	} else {
-		console.log(numberCount);
-	}	
+var userInput = (prompt('Please enter a number!'));
+	while (userInput % 1 != 0) {
+		userInput = (prompt('Please do not enter a decimal point'));
+	}
+	for (var numberCount = 1; numberCount <=userInput; numberCount++) {
+		if (numberCount % 5 === 0 && numberCount % 3 === 0){
+			document.write('FizzBuzz');
+			document.write('<br/>');
+		} else if (numberCount % 5 === 0) {
+			document.write('Buzz');
+			document.write('<br/>');
+		} else if (numberCount % 3 === 0){
+			document.write('Fizz');
+			document.write('<br/>');
+		} else {
+			document.write(numberCount);
+			document.write('<br/>');
+	};
 }
